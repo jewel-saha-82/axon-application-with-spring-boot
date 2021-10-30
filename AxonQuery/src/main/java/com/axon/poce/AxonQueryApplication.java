de9c722ecdac5ad9a5ef6b399d877f589df6b663
jewel-saha-82 <rdtipl@hotmail.com>
2021-10-30 17:41:56 +0530

    axon query

---
+++ AxonQuery/src/main/java/com/axon/poce/AxonQueryApplication.java
@@ -0,0 +1,13 @@
+package com.axon.poce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonQueryApplication.class, args);
+	}
+
+}
---
+++ AxonQuery/src/test/java/com/axon/poce/AxonQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axon.poce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxonQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
