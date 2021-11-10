db9c38504b8dfef2c7be155959de8592e523b49e
jewel-saha-82 <rdtipl@hotmail.com>
2021-11-10 11:39:45 +0530

    2nd event handler

--- AxonQuery/src/test/java/org/axon/poc/event/handler/test/EventHandlerTest.java
+++ AxonQuery/src/test/java/org/axon/poc/event/handler/test/EventHandlerTest.java
@@ -1,6 +1,7 @@
 package org.axon.poc.event.handler.test;
 
 import com.axon.common.events.cart.ItemAddedEvent;
+import com.axon.common.events.cart.ItemRemovedEvent;
 import org.assertj.core.api.BDDAssertions;
 import org.axon.poc.query.api.cart.handlers.CartHandlers;
 import org.axon.poc.query.api.cart.repository.CartRepository;
@@ -16,6 +17,23 @@ public class EventHandlerTest {
     @Test
     void itemAddedEventTest() {
         ItemAddedEvent event = ItemAddedEvent.builder()
+                .cartId("12345")
+                .item("PenDrive")
+                .quantity(3)
+                .build();
+
+        ArgumentCaptor<String> a1 = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> a2 = ArgumentCaptor.forClass(String.class);
+        cartHandlers.on(event);
+        Mockito.verify(repo).fetchByIdAndItem(a1.capture(), a2.capture());
+        BDDAssertions.then(a1.getValue()).isEqualTo("12345");
+        BDDAssertions.then(a2.getValue()).isEqualTo("PenDrive");
+
+    }
+
+    @Test
+    void itemRemovedEventTest() {
+        ItemRemovedEvent event = ItemRemovedEvent.builder()
                 .cartId("12345")
                 .item("PenDrive")
                 .quantity(3)
